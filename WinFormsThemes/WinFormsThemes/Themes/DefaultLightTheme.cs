24cef21c0a1878c514764a83ebd8dd5dd638a680
13997737+wolframhaussig@users.noreply.github.com <13997737+wolframhaussig@users.noreply.github.com>
2023-07-24 17:49:33 +0200

    removed obsolete TODO

--- WinFormsThemes/WinFormsThemes/Themes/DefaultLightTheme.cs
+++ WinFormsThemes/WinFormsThemes/Themes/DefaultLightTheme.cs
@@ -5,8 +5,6 @@ namespace WinFormsThemes.Themes
     /// <summary>
     /// Implementation of a light theme. Depends on the default colors of forms
     /// </summary>
-    //TODO: Currently, this does not switch back from dark mode as we depend on the colors of the forms
-    //and just use this theme to colorize warnings and so on
     public class DefaultLightTheme : AbstractTheme
     {
         public const string THEME_NAME = "LIGHT_DEFAULT_BUILTIN";
