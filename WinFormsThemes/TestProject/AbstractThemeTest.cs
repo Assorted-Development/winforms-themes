465faf7a44b7389df06d564d79c0a7ae90ad8935
13997737+wolframhaussig@users.noreply.github.com <13997737+wolframhaussig@users.noreply.github.com>
2023-07-26 22:12:06 +0200

    added AbstractTheme tests

---
+++ WinFormsThemes/TestProject/AbstractThemeTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Forms;
+using WinFormsThemes;
+using WinFormsThemes.Themes;
+
+namespace TestProject
+{
+    [TestClass]
+    public class AbstractThemeTest
+    {
+        [TestMethod]
+        public void PluginShouldBeCalledForExactType()
+        {
+            var plugin = new ThemePlugin();
+            var registry = IThemeRegistry.BUILDER
+                            .AddThemePlugin<Button>(plugin)
+                            .Build();
+            var button = new Button();
+            registry.Current?.Apply(button);
+            Assert.IsTrue(plugin.WasCalled);
+        }
+
+        [TestMethod]
+        public void PluginShouldNotBeCalledForDifferentType()
+        {
+            var plugin = new ThemePlugin();
+            var registry = IThemeRegistry.BUILDER
+                            .AddThemePlugin<Button>(plugin)
+                            .Build();
+            var form = new Form();
+            registry.Current?.Apply(form);
+            Assert.IsFalse(plugin.WasCalled);
+        }
+
+        [TestMethod]
+        public void PluginShouldNotBeCalledForSubType()
+        {
+            var plugin = new ThemePlugin();
+            var registry = IThemeRegistry.BUILDER
+                            .AddThemePlugin<Button>(plugin)
+                            .Build();
+            var button = new MyCustomButton();
+            registry.Current?.Apply(button);
+            Assert.IsFalse(plugin.WasCalled);
+        }
+
+        private class MyCustomButton : Button
+        { }
+
+        private class ThemePlugin : IThemePlugin
+        {
+            public bool WasCalled { get; private set; }
+
+            public void Apply(Control control, AbstractTheme theme)
+            {
+                WasCalled = true;
+            }
+        }
+    }
+}
