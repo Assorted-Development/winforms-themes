3f060d53923d9ff18b0ea0e052ebd9d4b53de316
13997737+wolframhaussig@users.noreply.github.com <13997737+wolframhaussig@users.noreply.github.com>
2023-07-26 21:58:18 +0200

    Added ThemeRegistryBuilder Test

---
+++ WinFormsThemes/TestProject/ThemeRegistryBuilderTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+using WinFormsThemes;
+using WinFormsThemes.Themes;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ThemeRegistryBuilderTest
+    {
+        [TestMethod]
+        public void AddingThemePluginsShouldWork()
+        {
+            var registry = IThemeRegistry.BUILDER
+                            .AddThemePlugin<Button>(new ThemePlugin())
+                            .Build();
+            Assert.AreEqual(1, registry.Current?.ThemePlugins?.Count);
+            Assert.AreEqual(typeof(ThemePlugin), registry.Current?.ThemePlugins?[typeof(Button)]?.GetType());
+        }
+
+        [TestMethod]
+        public void DefaultsShouldBeAddedWhenNotSet()
+        {
+            var registry = IThemeRegistry.BUILDER
+                            .Build();
+            Assert.IsTrue(registry.ListNames().Contains(DefaultDarkTheme.THEME_NAME));
+            Assert.IsTrue(registry.ListNames().Contains(DefaultLightTheme.THEME_NAME));
+            Assert.IsTrue(registry.ListNames().Contains(HighContrastDarkTheme.THEME_NAME));
+        }
+
+        [TestMethod]
+        public void ThemeRegistryHolderShouldBeSet()
+        {
+            var registry = IThemeRegistry.BUILDER
+                            .Build();
+            Assert.AreEqual(registry, ThemeRegistryHolder.ThemeRegistry);
+        }
+
+        private class ThemePlugin : IThemePlugin
+        {
+            public void Apply(Control control, AbstractTheme theme)
+            { }
+        }
+    }
+}
