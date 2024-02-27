5c7f8ac43a97de3d867750b95d139aa18924c666
wolframhaussig <13997737+wolframhaussig@users.noreply.github.com>
2024-02-27 20:25:37 +0100

    fix example form (#36)

--- WinFormsThemes/WinFormsThemes.Example/FrmDefault.cs
+++ WinFormsThemes/WinFormsThemes.Example/FrmDefault.cs
@@ -7,8 +7,7 @@ namespace StylableWinFormsControls.Example
         public FrmDefault()
         {
             InitializeComponent();
-            //ThemeRegistryHolder.ThemeRegistry.Current.Apply(this);
-            ThemeRegistryHolder.ThemeRegistry.Get(ThemeCapabilities.HighContrast).Apply(this);
+            ThemeRegistryHolder.ThemeRegistry!.GetTheme(ThemeCapabilities.HighContrast)?.Apply(this);
         }
 
         private void stylableListView1_SelectedIndexChanged(object sender, EventArgs e)
